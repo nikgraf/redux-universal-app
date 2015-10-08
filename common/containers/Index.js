af1d9b5a185fd76d21e3d3c9146a145a8042e2bf
Nik Graf <nik@nikgraf.com>
2015-10-08 13:35:39 +0200

    feat(Header): add example for demo purposes

--- common/containers/Index.js
+++ common/containers/Index.js
@@ -4,6 +4,30 @@ import AddMember from '../components/AddMember';
 import Member from '../components/Member';
 import { addMember, fetchMembers } from '../actions/MemberActions';
 
+const UserInfoA = () =>
+  <div>
+    <img src="https://avatars1.githubusercontent.com/u/223045?v=3&s=50" /> Nik
+  </div>
+;
+
+const UserInfoB = () =>
+  <div>
+    <img src="https://avatars1.githubusercontent.com/u/223045?v=3&s=50" /> Niko
+  </div>
+;
+
+const HeaderA = () =>
+  <div>
+    Search Header A <UserInfoA />
+  </div>
+;
+
+const HeaderB = () =>
+  <div>
+    Search Header B <UserInfoB />
+  </div>
+;
+
 class Index extends Component {
 
   static propTypes = {
@@ -11,14 +35,33 @@ class Index extends Component {
     dispatch: PropTypes.func.isRequired,
   }
 
+  state = {
+    searchActive: false,
+  }
+
   componentWillMount() {
     this.props.dispatch(fetchMembers());
+  }
+
+  _triggerToggleSearch() {
+    this.setState({ searchActive: !this.state.searchActive });
+  }
+
+  _renderHeader() {
+    if (this.state.searchActive) {
+      return <HeaderA />;
+    }
+
+    return <HeaderB />;
   }
 
   render() {
     const {dispatch, members} = this.props;
     return (
       <div>
+        { this._renderHeader() }
+        <button onClick={ ::this._triggerToggleSearch }>Toggle Header</button>
+
         <h1>Members</h1>
 
         <div>
